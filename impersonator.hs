f25483d16fb5ce937776bf53acfb6d9c89ac7e9e
rickyma <mr.rickyma@gmail.com>
2019-10-05 23:13:09 -0700

    Added sortByFreq to sort words from most to least frequent for easier debugging in the future

--- impersonator.hs
+++ impersonator.hs
@@ -1,5 +1,7 @@
 import Data.Char
 import Data.Map
+import Data.List 
+import Data.Function
 
 
 -- splitsep (==',') "3,5,"  => ["3","5",""]
@@ -13,9 +15,12 @@ removePunc string = [ char | char <- string, not (char `elem` ",.?!-:;\"\'\n") ]
 
 delPuncAndLower strings = Prelude.map (Prelude.map toLower) (Prelude.map removePunc strings)
 
+sortByFreq tuples = sortBy (flip compare `on` (\(a,b)->b)) tuples
+
 readtxt filename =
     do
       file <- readFile filename
       let words = (concat [splitsep (==' ') line| line <- splitsep (=='\n') file])
       let cleanWords = (delPuncAndLower words)
-      return (fromListWith (+) [(word, 1) | word <- cleanWords])
+      let tuples = toList (fromListWith (+) [(word, 1) | word <- cleanWords])
+      return (sortByFreq tuples)
