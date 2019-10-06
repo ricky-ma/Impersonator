9c87f5348263036c4eed8d3e6e2863016bcfd78f
Justin Frank <justinpfrank@protonmail.com>
2019-10-06 07:30:48 -0700

    Some import changes and added types to functions

--- impersonator.hs
+++ impersonator.hs
@@ -1,26 +1,30 @@
-import Data.Char
-import Data.Map
-import Data.List 
-import Data.Function
+import Data.Char (toLower)
+import Data.Map (Map, toList, fromListWith)
+import Data.List (sortBy)
+import Data.Function (on)
 
 
 -- splitsep (==',') "3,5,"  => ["3","5",""]
+splitsep :: (a -> Bool) -> [a] -> [[a]]
 splitsep sep [] = [[]]
 splitsep sep (h:t)
     | sep h = []: splitsep sep t
     | otherwise = ((h:w):rest)
                 where w:rest = splitsep sep t
 
+removePunc :: String -> String
 removePunc string = [ char | char <- string, not (char `elem` ",.?!-:;\"\'\n") ]
 
-delPuncAndLower strings = Prelude.map (Prelude.map toLower) (Prelude.map removePunc strings)
+delPuncAndLower :: [String] -> [String]
+delPuncAndLower strings = map (map toLower) (map removePunc strings)
 
+sortByFreq :: Ord n => [(t, n)] -> [(t, n)]
 sortByFreq tuples = sortBy (flip compare `on` (\(a,b)->b)) tuples
 
 readtxt filename =
     do
       file <- readFile filename
-      let words = (concat [splitsep (==' ') line| line <- splitsep (=='\n') file])
-      let cleanWords = (delPuncAndLower words)
+      let words = concat [splitsep (==' ') line| line <- splitsep (=='\n') file]
+      let cleanWords = delPuncAndLower words
       let tuples = toList (fromListWith (+) [(word, 1) | word <- cleanWords])
       return (sortByFreq tuples)
