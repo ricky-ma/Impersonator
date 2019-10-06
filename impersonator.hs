c9bd9d9f6f5a5f99a8e810671c6db4897228beba
Justin Frank <justinpfrank@protonmail.com>
2019-10-06 08:26:26 -0700

    Added n-gram extraction and sorting

--- impersonator.hs
+++ impersonator.hs
@@ -21,10 +21,17 @@ delPuncAndLower strings = map (map toLower) (map removePunc strings)
 sortByFreq :: Ord n => [(t, n)] -> [(t, n)]
 sortByFreq tuples = sortBy (flip compare `on` (\(a,b)->b)) tuples
 
-readtxt filename =
+ngrams :: [a] -> Int -> [[a]]
+ngrams lst n
+    | length lst >= n = take n lst : ngrams (tail lst) n
+    | otherwise = []
+
+readtxt :: (Ord n, Num n) => FilePath -> Int -> IO [[([String], n)]]
+readtxt filename n =
     do
       file <- readFile filename
       let words = concat [splitsep (==' ') line| line <- splitsep (=='\n') file]
       let cleanWords = delPuncAndLower words
-      let tuples = toList (fromListWith (+) [(word, 1) | word <- cleanWords])
-      return (sortByFreq tuples)
+      let grams = map (ngrams cleanWords) [1..n]
+      let tuples = map (\ xgrams -> (toList (fromListWith (+) [(gram, 1) | gram <- xgrams]))) grams
+      return (map sortByFreq tuples)
