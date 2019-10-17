93ce650739b211deedf6057e7eecee2d7c7e1132
Justin Frank <justinpfrank@protonmail.com>
2019-10-16 18:13:45 -0700

    Moved project to Cabal, changed to HashMap, improved ngrams performance

--- Impersonator.hs
+++ Impersonator.hs
@@ -1,18 +1,19 @@
 import Data.Char (toLower)
 import Data.Map (Map)
--- import Data.HashMap.Lazy (HashMap)
+import Data.HashMap.Strict (HashMap)
 import Data.Maybe (fromMaybe)
 import Data.List (sortBy)
 import Data.Function (on)
+import System.Environment (getArgs)
 
 import Data.Hashable
 
 import qualified Data.Map as M
--- import qualified Data.HashMap.Lazy as HM
+import qualified Data.HashMap.Strict as HM
 
 type Gram a = ([a], a)
 type GramPred a = (a, Int)
-type GramMap a = Map [a] (Map a Int)
+type GramMap a = HashMap [a] (Map a Int)
 
 -- splitsep (==',') "3,5,"  => ["3","5",""]
 splitsep :: (a -> Bool) -> [a] -> [[a]]
@@ -23,7 +24,7 @@ splitsep sep (h:t)
                 where w:rest = splitsep sep t
 
 removePunc :: String -> String
-removePunc string = [ char | char <- string, not (char `elem` ",.?!-:;\"\'\n") ]
+removePunc string = [ char | char <- string, not (char `elem` ",.?!-_:;\"\'\n") ]
 
 delPuncAndLower :: [String] -> [String]
 delPuncAndLower strings = map (map toLower) (map removePunc strings)
@@ -31,15 +32,20 @@ delPuncAndLower strings = map (map toLower) (map removePunc strings)
 sortByFreq :: Ord n => [(t, n)] -> [(t, n)]
 sortByFreq tuples = sortBy (flip compare `on` (\(a,b)->b)) tuples
 
+listHasN :: Int -> [a] -> Bool
+listHasN 0 _ = True
+listHasN n [] = False
+listHasN n (h:t) = listHasN (n-1) t
+
 ngrams :: [a] -> Int -> [Gram a]
 ngrams lst n
-    | length lst >= n = (take (n-1) lst, lst !! (n-1)) : ngrams (tail lst) n
+    | listHasN n lst = (take (n-1) lst, lst !! (n-1)) : ngrams (tail lst) n
     | otherwise = []
 
-insertGram :: (Ord a) => Gram a -> GramMap a -> GramMap a
-insertGram (key,next) = M.alter (Just . M.insertWith (+) next 1 . fromMaybe M.empty) key
+insertGram :: (Ord a, Hashable a) => Gram a -> GramMap a -> GramMap a
+insertGram (key,next) = HM.alter (Just . M.insertWith (+) next 1 . fromMaybe M.empty) key
 
-insertGrams :: (Ord a) => GramMap a -> [Gram a] -> GramMap a
+insertGrams :: (Ord a, Hashable a) => GramMap a -> [Gram a] -> GramMap a
 insertGrams = foldl (\gm g -> insertGram g gm)
 
 expandPredictions :: [GramPred a] -> [a]
@@ -47,9 +53,9 @@ expandPredictions [] = []
 expandPredictions ((v, 0):t) = expandPredictions t
 expandPredictions ((v, f):t) = v : (expandPredictions ((v, f-1):t))
 
-predictNext :: Ord a => GramMap a -> [a] -> [GramPred a]
+predictNext :: (Ord a, Hashable a) => GramMap a -> [a] -> [GramPred a]
 predictNext gm lst =
-  case M.lookup lst gm of
+  case HM.lookup lst gm of
     Just fm -> sortByFreq $ M.toList fm
     Nothing -> []
 
@@ -60,11 +66,13 @@ readtxt filename n =
       let words = concat [splitsep (==' ') line| line <- splitsep (=='\n') file]
       let cleanWords = delPuncAndLower words
       let grams = ngrams cleanWords n
-      let gramMap = insertGrams M.empty grams
+      let gramMap = insertGrams HM.empty grams
 
       return gramMap
 
--- main :: IO ()
--- main = do
---   [filename, n] <- getArgs
+main :: IO ()
+main = do
+  [filename] <- getArgs
+  gramMap <- readtxt filename 3
+  print $ predictNext gramMap ["whan", "that"]
 
